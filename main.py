5083cd0d587215eb375e27c403a2daf06b2012b9
Philamer Sune <phil@pez.ai>
2017-05-26 13:06:45 +0800

    Use readable var names.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit, Namespace
+from flask_socketio import Namespace, SocketIO, emit
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -22,18 +23,19 @@ class MyCustomNamespace(Namespace):
     def on_my_broadcast_event(self, message):
         emit('my_response', {'data': message['data']}, broadcast=True)
 
-t = MyCustomNamespace('/test')
-socketio.on_namespace(t)
+ns = MyCustomNamespace('/test')
+socketio.on_namespace(ns)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/cb')
-def cb():
-    socketio.emit('my_response', {'data': t.room}, namespace='/test', room=t.room)
-    return '200 OK'
+@app.route('/webhook')
+def webhook():
+    data = {'data': 'This message is initiated from server.'}
+    socketio.emit('my_response', data, namespace='/test', room=ns.room)
+    return 'OK'
 
 
 if __name__ == '__main__':
