196214e3af95ca33f7e225aa3e6bf9a60f952d1b
Philamer Sune <phil@pez.ai>
2017-05-26 13:02:30 +0800

    Send from server to client with specific room.

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, Namespace
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, engineio_logger=True)
+
+
+class MyCustomNamespace(Namespace):
+    room = None
+
+    def on_connect(self):
+        self.room = request.sid
+        emit('my_response', {'data': 'Connected'})
+
+    def on_disconnect(self):
+        print('Client disconnected')
+
+    def on_my_event(self, message):
+        emit('my_response', {'data': self.room})
+
+    def on_my_broadcast_event(self, message):
+        emit('my_response', {'data': message['data']}, broadcast=True)
+
+t = MyCustomNamespace('/test')
+socketio.on_namespace(t)
 
 
 @app.route('/')
@@ -12,24 +32,8 @@ def index():
 
 @app.route('/cb')
 def cb():
-   socketio.emit('my_response', {'data': 42}, namespace='/test')
-   return '200 OK'
-
-
-class MyCustomNamespace(Namespace):
-    def on_connect(self):
-        emit('my_response', {'data': 'Connected'})
-
-    def on_disconnect(self):
-        print('Client disconnected')
-
-    def on_my_event(self, message):
-        emit('my_response', {'data': message['data']})
-
-    def on_my_broadcast_event(self, message):
-        emit('my_response', {'data': message['data']}, broadcast=True)
-
-socketio.on_namespace(MyCustomNamespace('/test'))
+    socketio.emit('my_response', {'data': t.room}, namespace='/test', room=t.room)
+    return '200 OK'
 
 
 if __name__ == '__main__':
